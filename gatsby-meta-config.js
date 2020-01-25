af99cac8b7d32227bec24e9f7e9029c628d89779
andres alcocer <andresalcocer7@yahoo.com>
2020-01-25 18:15:53 -0500

    inital commit

---
+++ gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: `Andres Alcocer - Front End Dev`,
+  description: `Blog posted about ...`,
+  author: `Andres Alcocer`,
+  introduction: `I am a front end developer who loves making the internet a fun, pretty, and engaging place.`,
+  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  social: {
+    twitter: `andresLM10`, // Your Twitter account
+    github: `AndresXI`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account,
+    linkedin: `https://www.linkedin.com/in/andres-io/`
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'jbee',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '', // Add your google analytics tranking ID
+}
---
+++ src/constants/enum.js
@@ -0,0 +1,8 @@
+export const CATEGORY_TYPE = {
+  ALL: 'All',
+}
+
+export const THEME = {
+  DARK: 'dark',
+  LIGHT: 'light',
+}
